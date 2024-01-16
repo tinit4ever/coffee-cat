53d4ce0b127825a8ebb85214b53574789f97fa28
tinit4ever <tinit4ever@gmail.com>
2024-01-16 09:37:37 +0700

    create iOS project schema

--- coffee cat/Views/GetStartedViewController.swift
+++ coffee cat/Views/GetStartedViewController.swift
@@ -7,13 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class GetStartedViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
