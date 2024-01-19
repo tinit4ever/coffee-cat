6cc53ad2110c421cd08ecd6a1de42d316d4d8eb1
tinit4ever <tinit4ever@gmail.com>
2024-01-19 09:30:26 +0700

    seperate UI component replace background + finish group circle darkmore

---
+++ coffee cat/Subviews/CircleView.swift
@@ -0,0 +1,20 @@
+//
+//  CircleView.swift
+//  coffee cat
+//
+//  Created by Tin on 19/01/2024.
+//
+
+import UIKit
+
+class CircleView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
