814c9141dc7496818bd1777f435a37dece478fb7
tinit4ever <tinit4ever@gmail.com>
2024-01-31 16:52:32 +0700

    add background

--- coffee-cat/coffee-cat/Views/Views/HomeViewController.swift
+++ coffee-cat/coffee-cat/Views/Views/HomeViewController.swift
@@ -12,13 +12,83 @@ class HomeViewController: UIViewController {
     let heightScaler = UIScreen.scalableHeight
     let widthScaler = UIScreen.scalableWidth
     let sizeScaler = UIScreen.scalableSize
+    // -MARK: Create UI Components
 
+    // -MARK: ViewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
     }
     
+    // -MARK: Override
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        super.traitCollectionDidChange(previousTraitCollection)
+        
+        if traitCollection.hasDifferentColorAppearance(comparedTo: previousTraitCollection) {
+            configAppearance()
+        }
+    }
+    
+    // -MARK: SetupUI
     private func setupUI() {
+        configAppearance()
+        
+        configNavigation()
+    }
+    
+    private func configAppearance() {
+        view.backgroundColor = .systemGray5
+        
+        removeCircleGroupImageView()
+        checkAndChangeAppearancceMode()
+    }
+    
+    private func removeCircleGroupImageView() {
+        for subview in view.subviews {
+            if let imageView = subview as? UIImageView,
+               let imageName = imageView.image?.accessibilityIdentifier,
+               (imageName == ImageNames.darkCircleGroup || imageName == ImageNames.lightCircleGroup)
+            {
+                imageView.removeFromSuperview()
+            }
+        }
+    }
+    
+    private func checkAndChangeAppearancceMode() {
+        if traitCollection.userInterfaceStyle == .dark {
+            let image = UIImage(named: ImageNames.darkCircleGroup)
+            image?.accessibilityIdentifier = ImageNames.darkCircleGroup
+
+            let resizedImage = image?.resized(to: CGSize(width: widthScaler(700), height: heightScaler(200)))
+
+            let imageView = UIImageView(image: resizedImage)
+            imageView.image?.accessibilityIdentifier = ImageNames.darkCircleGroup
+
+            view.addSubview(imageView)
+
+        } else {
+            let image = UIImage(named: ImageNames.darkCircleGroup)
+            image?.accessibilityIdentifier = ImageNames.darkCircleGroup
+
+            let resizedImage = image?.resized(to: CGSize(width: widthScaler(700), height: heightScaler(200)))
+
+            let imageView = UIImageView(image: resizedImage)
+            imageView.image?.accessibilityIdentifier = ImageNames.darkCircleGroup
+
+            view.addSubview(imageView)
+
+        }
+    }
+    
+    private func configNavigation() {
+        let backImage = UIImage(systemName: "chevron.backward.circle.fill")?
+            .withTintColor(.backButton, renderingMode: .alwaysOriginal)
+            .resized(to: CGSize(width: sizeScaler(50), height: sizeScaler(50)))
+        
+        self.navigationController?.navigationBar.backIndicatorImage = backImage
+        self.navigationController?.navigationBar.backIndicatorTransitionMaskImage = backImage
+        self.navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+        self.navigationItem.backBarButtonItem?.tintColor = .backButton
     }
 }
 
