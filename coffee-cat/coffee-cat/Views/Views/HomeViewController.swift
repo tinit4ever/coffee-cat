d5f33f15a5d0bebf659dac34fd7e76bd7448ec31
tinit4ever <tinit4ever@gmail.com>
2024-01-31 09:34:20 +0700

    restruct Views package

---
+++ coffee-cat/coffee-cat/Views/Views/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  HomeViewController.swift
+//  coffee-cat
+//
+//  Created by Tin on 31/01/2024.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+    
+    private func setupUI() {
+        view.backgroundColor = .systemMint
+    }
+}
