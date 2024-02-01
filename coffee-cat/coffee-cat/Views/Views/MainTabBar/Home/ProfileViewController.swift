d22b445c2929ddcafa231af632581676f83696dc
tinit4ever <tinit4ever@gmail.com>
2024-02-01 16:20:44 +0700

    create view controller that needed and change name old view controller

---
+++ coffee-cat/coffee-cat/Views/Views/MainTabBar/Home/ProfileViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ProfileViewController.swift
+//  coffee-cat
+//
+//  Created by Tin on 01/02/2024.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
