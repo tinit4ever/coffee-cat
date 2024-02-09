775caefd0ee2141725ca54ce5a97dacda2350e27
tinit4ever <tinit4ever@gmail.com>
2024-02-09 17:16:12 +0700

    resolve conflict

---
+++ coffee-cat/coffee-cat/Extensions/UITabBar+Extension.swift
@@ -0,0 +1,20 @@
+//
+//  UITabBar+Extension.swift
+//  coffee-cat
+//
+//  Created by Tin on 01/02/2024.
+//
+
+import UIKit
+
+extension UITabBar {
+    override open func sizeThatFits(_ size: CGSize) -> CGSize {
+        super.sizeThatFits(size)
+        guard let window = UIApplication.shared.windows.filter {$0.isKeyWindow}.first else {
+            return super.sizeThatFits(size)
+        }
+        var sizeThatFits = super.sizeThatFits(size)
+        sizeThatFits.height = window.safeAreaInsets.bottom + 40
+        return sizeThatFits
+    }
+}
