82d984922bfb7f1aadb1179b6a3dc7b2077f9f2c
Thanh Long Đặng <dangthanhlong211@gmail.com>
2024-02-16 13:39:46 +0700

    fix:register dob

--- ccp/src/main/java/com/swd/ccp/models/response_models/AccountResponse.java
+++ ccp/src/main/java/com/swd/ccp/models/response_models/AccountResponse.java
@@ -23,7 +23,7 @@ public class AccountResponse {
 
     private String gender;
 
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private java.sql.Date dob;
 
     private String status;
