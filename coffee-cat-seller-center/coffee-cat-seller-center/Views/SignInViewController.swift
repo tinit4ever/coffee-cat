23da1fe783cb70e87540636e84d68221714960b7
tinit4ever <tinit4ever@gmail.com>
2024-02-26 21:32:08 +0700

    init administrator project

---
+++ coffee-cat-seller-center/coffee-cat-seller-center/Views/SignInViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SignInViewController.swift
+//  coffee-cat-seller-center
+//
+//  Created by Tin on 26/02/2024.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+    // MARK: - ViewDidLoad
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+    
+    // MARK: - setupUI
+    private func setupUI() {
+        view.backgroundColor = .systemMint
+    }
+}
